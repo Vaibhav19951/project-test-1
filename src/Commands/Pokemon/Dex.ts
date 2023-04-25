2e70718c20422287513397daefe912cf4c4a98b7
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2023-04-25 16:32:10 +0530

    Update Dex.ts

--- src/Commands/Pokemon/Dex.ts
+++ src/Commands/Pokemon/Dex.ts
@@ -5,7 +5,7 @@ import { IArgs } from '../../Types'
     description: 'dex',
     aliases: ['dex'],
     exp: 20,
-    cooldown: 15,
+    cooldown: 5,
     usage: '',
     category: 'pokemon'
 })
@@ -13,7 +13,7 @@ export default class command extends BaseCommand {
     override execute = async (M: Message): Promise<void> => {
         const { party, pc } = await this.client.DB.getUser(M.sender.jid)
         const pokemons = [...party, ...pc]
-        if (pokemons.length < 1) return void M.reply('no pokemon')
+        if (pokemons.length < 1) return void M.reply('nahi hai koi pokemon')
         let text = `*Pokedex*\n`
         pokemons.forEach((x) => (text += `\n*❯ ${this.client.utils.capitalize(x.name)}*`))
         return void (await M.reply(text))
